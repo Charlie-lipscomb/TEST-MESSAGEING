abffef45270bf8d11b215af17de623ae2a2e80c0
Charlie-lipscomb <Charlie_lips@icloud.com>
2025-10-30 15:30:04 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-self.addEventListener("install",e=>{self.skipWaiting()});
-self.addEventListener("activate",e=>{self.clients.claim()});
-self.addEventListener("push",e=>{
-  const data=e.data?.json()||{};
-  e.waitUntil(self.registration.showNotification(data.title||"Whisp",{body:data.body||"",icon:"https://cdn-icons-png.flaticon.com/512/906/906349.png"}));
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow("./"));
 });
